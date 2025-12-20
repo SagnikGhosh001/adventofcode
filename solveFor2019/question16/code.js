c30c45eb234c3ecea929003e325fe3bf50baeb14
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Pro.local>
2025-12-20 23:40:00 +0530

    solve part 2

--- solveFor2019/question16/code.js
+++ solveFor2019/question16/code.js
@@ -1,8 +1,8 @@
-// // const input = "12345678";
+// const input = "02935109699940807407585447034323";
 const input =
   "59749012692497360857047467554796667139266971954850723493205431123165826080869880148759586023048633544809012786925668551639957826693687972765764376332866751104713940802555266397289142675218037547096634248307260671899871548313611756525442595930474639845266351381011380170903695947253040751698337167594183713030695951696818950161554630424143678657203441568388368475123279525515262317457621810448095399401556723919452322199989580496608898965260381604485810760050252737127446449982328185199195622241930968611065497781555948187157418339240689789877555108205275435831320988202725926797165070246910002881692028525921087207954194585166525017611171911807360959";
-const pattern = [0, 1, 0, -1];
 
+const pattern = [0, 1, 0, -1];
 const makeRepeatationArray = (pos, length) => {
   const repeatationArray = [];
 
@@ -21,10 +21,8 @@ const getPhaseResult = (inputArray, repeatedValue) =>
     0,
   );
 
-const main = () => {
-  const largeInput = input.repeat(1);
-  const messageOffset = +input.slice(0, 7);
-  let inputArray = [...largeInput].map(Number);
+const part1 = () => {
+  let inputArray = [...input].map(Number);
 
   for (let time = 0; time < 100; time++) {
     const newStringArray = [];
@@ -39,8 +37,29 @@ const main = () => {
     inputArray = [...newStringArray];
   }
 
-  // console.log(messageOffset, inputArray.length, inputArray);
-  console.log(inputArray.slice(0, 0 + 8));
+  console.log(inputArray.slice(0, 8).join(""));
 };
 
-main();
+part1();
+
+const part2 = () => {
+  const largeInput = input.repeat(10000);
+  const messageOffset = +input.slice(0, 7);
+  const inputArray = [...largeInput].map(Number);
+
+  let relevantInput = inputArray.slice(messageOffset);
+
+  for (let time = 0; time < 100; time++) {
+    const newArray = [];
+    let sum = 0;
+    for (let i = relevantInput.length - 1; i >= 0; i--) {
+      sum += relevantInput[i];
+      newArray[i] = Math.abs(sum) % 10;
+    }
+    relevantInput = [...newArray];
+  }
+
+  console.log(relevantInput.slice(0, 8).join(""));
+};
+
+part2();
